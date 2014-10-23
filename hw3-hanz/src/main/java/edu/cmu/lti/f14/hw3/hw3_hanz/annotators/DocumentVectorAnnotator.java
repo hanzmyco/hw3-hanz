bd914e3c381e26e62c8e69455123e80ed631aec3
‘michaelzhhan1 <hanz@cs.cmu.edu>
2014-10-23 00:45:24 -0400

    2nd

--- hw3-hanz/src/main/java/edu/cmu/lti/f14/hw3/hw3_hanz/annotators/DocumentVectorAnnotator.java
+++ hw3-hanz/src/main/java/edu/cmu/lti/f14/hw3/hw3_hanz/annotators/DocumentVectorAnnotator.java
@@ -1,5 +1,9 @@
 package edu.cmu.lti.f14.hw3.hw3_hanz.annotators;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
@@ -11,6 +15,9 @@ import org.apache.uima.jcas.cas.StringArray;
 import org.apache.uima.jcas.tcas.Annotation;
 
 import edu.cmu.lti.f14.hw3.hw3_hanz.typesystems.Document;
+import edu.cmu.lti.f14.hw3.hw3_hanz.typesystems.Token;
+import edu.cmu.lti.f14.hw3.hw3_hanz.utils.StanfordLemmatizer;
+import edu.cmu.lti.f14.hw3.hw3_hanz.utils.Utils;
 
 public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 
@@ -40,9 +47,47 @@ public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 	    res.add(s);
 	  return res;
 	}
+	
+	/*
+	 * use stopwords, stems, lowercase to improve token vector
+	 */
+	List<String> tokenize_hanz(String doc) throws FileNotFoundException {
+
+    List<String> res = new ArrayList<String>();
+    BufferedReader br = new BufferedReader(new FileReader("stopwords.txt"));
+    HashSet<String> word_list = new HashSet<String>();
+    String line = "";
+    
+    try {
+      while ((line = br.readLine()) != null)
+        word_list.add(line);
+      br.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    for (String s : doc.split("\\s+")) {
+      /**
+       * normalize
+       */
+      String tmp = s.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+      /**
+       * stemming
+       */
+      String stemword = StanfordLemmatizer.stemWord(tmp);
+      /**
+       * get rid of stop words
+       */
+      
+      if (!word_list.contains(stemword))
+        res.add(stemword);
+    }
+
+    return res;
+  }
 
 	/**
-	 * 
+	 * This will get the word vector in each document
 	 * @param jcas
 	 * @param doc
 	 */
@@ -53,7 +98,32 @@ public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 		
 		//TO DO: construct a vector of tokens and update the tokenList in CAS
     //TO DO: use tokenize0 from above 
-		
+		List<String> a=new ArrayList<String>();
+		a=tokenize0(docText);
+		HashMap<String, Token> t=new HashMap<String, Token>();
+		/**
+		 * get all the frequency
+		 */
+		for(int i=0;i<a.size();i++){
+		  String word=a.get(i);
+		  Token tt=t.get(word);
+		  if(tt==null){
+		    Token t_new=new Token(jcas);
+		    t_new.setText(word);
+		    t_new.setFrequency(1);
+		    t.put(word, t_new);
+		  }
+		  else{
+		    tt.setFrequency(tt.getFrequency()+1);
+		    t.put(word, tt);
+		    
+		  }
+		}
+		ArrayList<Token>arr=new ArrayList<Token>();
+		for(String s : t.keySet()){
+		  arr.add(t.get(s));
+		}
+		doc.setTokenList(Utils.fromCollectionToFSList(jcas, arr));
 
 	}
 
--- hw3-hanz/src/main/java/edu/cmu/lti/f14/hw3/hw3_hanz/casconsumers/RetrievalEvaluator.java
+++ hw3-hanz/src/main/java/edu/cmu/lti/f14/hw3/hw3_hanz/casconsumers/RetrievalEvaluator.java
@@ -1,7 +1,12 @@
 package edu.cmu.lti.f14.hw3.hw3_hanz.casconsumers;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.uima.cas.CAS;
@@ -15,6 +20,9 @@ import org.apache.uima.resource.ResourceProcessException;
 import org.apache.uima.util.ProcessTrace;
 
 import edu.cmu.lti.f14.hw3.hw3_hanz.typesystems.Document;
+import edu.cmu.lti.f14.hw3.hw3_hanz.typesystems.Token;
+import edu.cmu.lti.f14.hw3.hw3_hanz.utils.Utils;
+
 
 
 public class RetrievalEvaluator extends CasConsumer_ImplBase {
@@ -24,18 +32,22 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 
 	/** query and text relevant values **/
 	public ArrayList<Integer> relList;
-
-		
+	public ArrayList<String> textList;
+	public ArrayList<HashMap<String,Double>>vec;
+	public ArrayList<HashMap<String,Double>>vc;
+	
 	public void initialize() throws ResourceInitializationException {
 
 		qIdList = new ArrayList<Integer>();
-
 		relList = new ArrayList<Integer>();
+		textList=new ArrayList<String>();
+		vec=new ArrayList<HashMap<String,Double>>();
+		vc=new ArrayList<HashMap<String,Double>>();
 
 	}
 
 	/**
-	 * TODO :: 1. construct the global word dictionary 2. keep the word
+	 * 1.construct the global word dictionary 2. keep the word
 	 * frequency for each sentence
 	 */
 	@Override
@@ -55,64 +67,242 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 
 			//Make sure that your previous annotators have populated this in CAS
 			FSList fsTokenList = doc.getTokenList();
-			//ArrayList<Token>tokenList=Utils.fromFSListToCollection(fsTokenList, Token.class);
+			String t=doc.getText();
+			textList.add(t);
+			
+			
+			ArrayList<Token>tokenList=Utils.fromFSListToCollection(fsTokenList, Token.class);
+			HashMap<String,Double>tmp=collectionToVector(tokenList);
+			vec.add(tmp);
+			vc.add(tmp);
 
 			qIdList.add(doc.getQueryID());
 			relList.add(doc.getRelevanceValue());
 			
-			//Do something useful here
 
 		}
 
 	}
 
 	/**
-	 * TODO 1. Compute Cosine Similarity and rank the retrieved sentences 2.
+	 * 1. Compute Cosine Similarity and rank the retrieved sentences 2.
 	 * Compute the MRR metric
 	 */
 	@Override
-	public void collectionProcessComplete(ProcessTrace arg0)
-			throws ResourceProcessException, IOException {
+	public void collectionProcessComplete(ProcessTrace arg0)throws ResourceProcessException, IOException {
 
 		super.collectionProcessComplete(arg0);
-
-		// TODO :: compute the cosine similarity measure
+		BufferedWriter writer=new BufferedWriter(new FileWriter("report.txt"));
+		ArrayList<Integer> rank=new ArrayList<Integer>();
+		for (int i = 0; i < qIdList.size();) {
+		  int qId = qIdList.get(i);
+		  int j = i + 1;
+		  HashMap<String, Double> queryVector = vec.get(i);
+		  ArrayList<Tuple> result = new ArrayList<Tuple>();
+		  
+		  while (j < qIdList.size() && qIdList.get(j) == qId) {
+		    HashMap<String, Double> docVector = vec.get(j);
+		    
+		    /**
+		     * we can use different similarity function here
+		     */
+		    
+		    //double score=bm25(queryVector,docVector,i)
+		    
+		    double score = computeCosineSimilarity(queryVector, docVector);
+		    Tuple pair = new Tuple(score, j);
+		    result.add(pair);
+		    j++;
+		  }
+		  
+		  /**
+		   * get the ranks
+		   */
+		  Collections.sort(result,new TupleComparator());
+		  for (int ii=0;ii<result.size();ii++){
+		    int ind=result.get(ii).index;
+		    if(relList.get(ind)==1){
+		      String cosine=String.format("%.4f", result.get(ii).score);
+		      writer.write("cosine="+cosine+"\track="+(ii+1)+"\tqid="+qId+"\trel=1\t"+textList.get(ind)+"\n");
+		      rank.add(ii+1);
+		      break;
+		    }
+		  }
+		  i=j;
+    
+		}
+		/*
+		 * comput mrr
+		 */
+		double metric_mrr = compute_mrr(rank);
+		String out=String.format("%.4f",metric_mrr);
+		writer.write("MRR="+out);
+		writer.close();
 		
-		
-		
-		// TODO :: compute the rank of retrieved sentences
-		
-		
-		
-		// TODO :: compute the metric:: mean reciprocal rank
-		double metric_mrr = compute_mrr();
-		System.out.println(" (MRR) Mean Reciprocal Rank ::" + metric_mrr);
+		//System.out.println(" (MRR) Mean Reciprocal Rank ::" + metric_mrr);
 	}
+	
+	private HashMap<String, Double> collectionToVector(ArrayList<Token> input) {
+    HashMap<String, Double> map = new HashMap<String, Double>();
+    for (Token token : input) {
+      map.put(token.getText(), (double) token.getFrequency());
+    }
+    return map;
+  }
 
 	/**
 	 * 
 	 * @return cosine_similarity
 	 */
-	private double computeCosineSimilarity(Map<String, Integer> queryVector,
-			Map<String, Integer> docVector) {
-		double cosine_similarity=0.0;
+	private double computeCosineSimilarity(Map<String, Double> queryVector,
+			Map<String, Double> docVector) {
+		double cosine_similarity = 0.0;
+    double queryLen = 0.0, docLen = 0.0, innerProd = 0.0;
 
-		// TODO :: compute cosine similarity between two sentences
-		
+    for (String key : queryVector.keySet())
+      queryLen += Math.pow(queryVector.get(key), 2);
 
-		return cosine_similarity;
+    for (String key : docVector.keySet()) {
+      docLen += Math.pow(docVector.get(key), 2);
+      if (queryVector.containsKey(key)) {
+        innerProd += docVector.get(key) * queryVector.get(key);
+      }
+    }
+
+    cosine_similarity = innerProd / Math.sqrt((queryLen * docLen));
+
+    return cosine_similarity;
 	}
 
 	/**
 	 * 
 	 * @return mrr
 	 */
-	private double compute_mrr() {
-		double metric_mrr=0.0;
-
-		// TODO :: compute Mean Reciprocal Rank (MRR) of the text collection
+	private double compute_mrr(ArrayList<Integer> rank) {
+	  double metric_mrr = 0.0;
+    for (int i = 0; i < rank.size(); i++) {
+      metric_mrr += 1.0 / (double) rank.get(i);
+    }
+    return metric_mrr / (double) rank.size();
 		
-		return metric_mrr;
+	
 	}
+	/**
+	 * basic bm25
+	 * @param queryVector
+	 * @param docVector
+	 * @param index
+	 * @return
+	 */
+	private double bm25(Map<String, Double> queryVector, Map<String, Double> docVector, int index) {
+
+    transformToIdf();
+    double score = 0.0;
+    double k1 = 1.2, b = 0.5;
+    int docLen = 0;
+
+    for (String key : docVector.keySet()) {
+      docLen += docVector.get(key);
+    }
+
+    for (String key : queryVector.keySet()) {
+      double tf = docVector.containsKey(key) ? docVector.get(key) : 0;
+      score += vc.get(index).get(key) * tf * (k1 + 1.0) / (tf + k1 * (1.0 - b + b * docLen));
+    }
+
+    return score;
+  }
+	
+	/*
+	 * this class is used to store the scores for each doc
+	 */
+	public class Tuple {
+    public double score;
+
+    public int index;
+
+    public Tuple(double score, int index) {
+      this.score = score;
+      this.index = index;
+    }
+  }
+	
+	/*
+	 * simple compare
+	 */
+	public class TupleComparator implements Comparator<Tuple> {
+    @Override
+    public int compare(Tuple p1, Tuple p2) {
+      if (p1.score > p2.score)
+        return -1;
+      else if (p1.score < p2.score)
+        return 1;
+      return 0;
+    }
+  }
+	/*
+   * this can be use in task2 to improve similarity
+   */
+	 public void transformTfIdf() {
+	    for (int i = 0; i < vec.size();) {
+	      HashMap<String, Double> df = new HashMap<String, Double>();
+	      int qId = qIdList.get(i);
+	      int j = i;
+	      
+	      while (j < qIdList.size() && qIdList.get(j) == qId) {
+	        HashMap<String, Double> vecc = vec.get(j);
+	        for (String key : vecc.keySet()) {
+	          if (df.containsKey(key))
+	            df.put(key, df.get(key) + 1);
+	          else
+	            df.put(key, 1.0);
+	        }
+	        j++;
+	      }
+
+	      int docSize = j - i + 1;
+	      for (int k = i; k < j; k++) {
+	        HashMap<String, Double> tmpVec = vec.get(k);
+	        for (String key : tmpVec.keySet()) {
+	          double docFreq = df.get(key);
+	          tmpVec.put(key, tmpVec.get(key) * Math.log10((double) docSize / docFreq));
+	        }
+	      }
+	      i = j;
+	    }
+	  }
+	 /*
+	  * use for bm25
+	  */
+	 public void transformToIdf() {
+	    for (int i = 0; i < vec.size();) {
+	      HashMap<String, Double> df = new HashMap<String, Double>();
+
+	      int qId = qIdList.get(i);
+	      int j = i;
+	      while (j < qIdList.size() && qIdList.get(j) == qId) {
+	        HashMap<String, Double> vecc = vec.get(j);
+	        for (String key : vecc.keySet()) {
+	          if (df.containsKey(key))
+	            df.put(key, df.get(key) + 1);
+	          else
+	            df.put(key, 1.0);
+	        }
+	        j++;
+	      }
+
+	      int docSize = j - i + 1;
+	      for (int k = i; k < j; k++) {
+	        HashMap<String, Double> tmpVec = vc.get(k);
+	        for (String key : tmpVec.keySet()) {
+	          double docFreq = df.get(key);
+	          tmpVec.put(key, Math.log10((double) docSize / docFreq));
+	        }
+	      }
+	      i = j;
+	    }
+	  }
+
+	
 
 }
